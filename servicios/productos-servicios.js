c094af76ce0b53924c2ebe1c98bd08f7c76c4575
Juan Cruz Leal <92988456+juansantafe7@users.noreply.github.com>
2022-08-03 15:16:03 -0300

    Delete servicios directory

--- servicios/productos-servicios.js
+++
@@ -1,9 +0,0 @@
-const listaProductos = () => fetch("http://localhost:3000/productos-puntoi").then(respuesta => respuesta.json());
-
-
-export const productoServices = {
-
-    listaProductos,
-
-}
-
